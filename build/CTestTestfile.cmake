bcdcacd3f50fd7c30b9b7badc53b025126d778c5
Ivan Zharov <s20b1zharov@gmail.com>
2024-09-13 22:09:56 +0200

    deleted build catalog

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/ivanz/prog/cpp/ascii_game
-# Build directory: C:/Users/ivanz/prog/cpp/ascii_game/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
-subdirs("tests")
--- build/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/ivanz/prog/cpp/ascii_game/src
-# Build directory: C:/Users/ivanz/prog/cpp/ascii_game/build/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
